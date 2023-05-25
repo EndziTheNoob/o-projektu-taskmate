2fb57c894ccf7740d3707e0a5bfd2dbb731b254e
Angelika Bauerová <angelika.bauerova@gmail.com>
2023-05-25 21:49:56 +0200

    uprava textu

--- src/About/about.jsx
+++ src/About/about.jsx
@@ -17,14 +17,10 @@ const About = () => {
         The minimalist design ensures a clutter-free interface, allowing you to
         concentrate on what truly matters: getting things done.
       </p>
-      <p>
-        <h3>Our TaskMate project prioritizes:</h3> usability, making it
-        accessible and enjoyable for users of all levels of technical expertise.{' '}
-        <h3>The interface is designed:</h3> with simplicity in mind, enabling
-        effortless navigation and quick task management.
-        <h3>Whether you're: </h3>a seasoned web user or new to this domain,
-        you'll find our application easy to understand and use.
-      </p>
+      <h3>Our TaskMate project prioritizes:</h3> Usability, making it accessible
+      and enjoyable for users of all levels of technical expertise.{' '}
+      <h3>The interface is designed:</h3> With simplicity in mind, enabling
+      effortless navigation and quick task management.
       <div className="about_img">
         <img src={screen2} className="screenshot" />
         <img src={screen1} className="screenshot" />
