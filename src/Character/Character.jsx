f0543b22b24d02d81f2463c2af412de65fd13740
Andrea Wimberská <awimberska@gmail.com>
2023-05-26 19:03:14 +0200

    zmena character css

--- src/Character/Character.jsx
+++ src/Character/Character.jsx
@@ -5,20 +5,22 @@ import taskmateGif from './img/Taskmate_Kafe.gif';
 const Character = () => {
   return (
     <div className="character">
-      <h1>Who is TaskMate?</h1>
-      <p>
-        {' '}
-        TaskMate showcases a delightful animated pixel art character that adds a
-        touch of whimsy to your daily tasks.
-      </p>
-      <p>
-        The character comes to life with fluid animations and charming
-        movements, creating an engaging and interactive experience for users.
-      </p>
-      <p>
-        It's a perfect blend of nostalgia and modern design, sure to bring a
-        smile to your face.
-      </p>
+      <div className="character-text">
+        <h1>Who is TaskMate?</h1>
+        <p>
+          {' '}
+          TaskMate showcases a delightful animated pixel art character that adds
+          a touch of whimsy to your daily tasks.
+        </p>
+        <p>
+          The character comes to life with fluid animations and charming
+          movements, creating an engaging and interactive experience for users.
+        </p>
+        <p>
+          It's a perfect blend of nostalgia and modern design, sure to bring a
+          smile to your face.
+        </p>
+      </div>
       <img src={taskmateGif} className="gif_kafe" />
     </div>
   );
